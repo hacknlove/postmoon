b70325e1201e753cc4b76754336872eeb3eb8d74
Glitch (hacknlove-postmoon) <none>
2019-07-19 16:58:18 +0000

    ❄️👰 Checkpoint ./tests/buscarSlotPorNombre/test.js:5616004/795

--- tests/buscarSlotPorNombre/test.js
+++ tests/buscarSlotPorNombre/test.js
@@ -2,15 +2,28 @@ var response = pm.response.json().data;
 
 pm.test("se devuelve el slot buscado por el nombre", function () {
   response.data.forEach(subslot => {
+//    if(!subslot.slot.client) {
+//      return 
+//    }
+    
     pm.expect(subslot.slot).to.have.property('client')
-    pm.expect(subslot.slot.client.name).to.be.eql("66666")    
+    pm.expect(subslot.slot.client.name).to.be.eql("99999")    
   })
 //  pm.expect(response.data.data.every(subslot => subslot.slot.client.name)).to.be.eql("99999") 
 
 });
 
 pm.test("se devuelve el slot buscado por el nombre", function () {
-    pm.expect(response.data.some(subslot => 
+  response.data.forEach(subslot => {
+    if(subslot.slot.client) {
+      pm.expect(subslot.slot.client.name).to.be.eql("99999")    
+    }
+  })
+});
+
+
+pm.test("se devuelve el slot buscado por el nombre", function () {
+    pm.expect(response.data.every(subslot => 
     
       subslot.slot.client.name == "99999")).to.be.true;
     
