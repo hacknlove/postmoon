5c9e481bf4346a70eb94eb6cd5695a0ec4347083
Glitch (hacknlove-postmoon) <none>
2019-07-25 19:16:04 +0000

    🐞🐛 Checkpoint ./profilesSlaType.js:5616004/66

--- profilesSlaType.js
+++ profilesSlaType.js
@@ -3,7 +3,9 @@
 //cambiar la id y "separeted" por algo (parece que solo hay 2 opciones)
 pm.test(" profilesSlaType de perfil se ha cambiado correctamente", function () {
     pm.expect(response.id).to.eql(pm.environment.get("PerfilId"));
-    pm.expect(response.profilesSlaType.id).to.eql("");
+    pm.expect(response.profilesSlaType.id).to.eql();
+    pm.expect(response.profilesSlaType.name).to.eql();
+
 
        
 });
