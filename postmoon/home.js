65328845f10796a6dce9a375fd3b18e5e63d2ac6
antonio <antonio@pykiss.com>
2019-07-28 11:42:09 +0200

    .

--- postmoon/home.js
+++ postmoon/home.js
@@ -2,7 +2,7 @@ const glob = require('glob')
 
 function home (req, res) {
   res.render('home', {
-    test: glob.sync('*.js').map(e => e.substr(0, e.length - 3))
+    test: glob.sync('*.js').map(e => e.substr(0, e.length - 3)).filter(e => !e.match(/\.pre$/))
   })
 }
 
